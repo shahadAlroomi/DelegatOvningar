1e5871b18a738921986d74a4becc21b7fd09829e
shahadAlroomi <shahad.ro@icloud.com>
2026-01-07 13:14:58 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Skapa en Action som skriver "Hej!" till konsolen och kör den.
+//Action sayHello = ???;
+//sayHello();
+
+
+Action sayHello = PrintWelcomeMessage ;
+
+static void PrintWelcomeMessage()
+{
+    Console.WriteLine("Hej!");
+}
+
+sayHello();
+
+//skapa en Action som tar emot ett namn och skriver "Hej, [namn]!" till konsolen. Kör delegaten två gånger med två olika namn.
+
+Action<string> messageWithName = n => Console.WriteLine("Hej "+ n);
+
+messageWithName ("shahad");
+messageWithName("Elin");
